88900a97662c1676e16799609cc3f336c5aa93bb
TheElegantCoder <katharina.rychnavska@gmail.com>
2020-07-05 11:03:08 +0000

    Here is my first loop! Yey!!!

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # function with arguments, multiple arguments and loops 
-# for x in range(10)
-# print x
+# for x in range(10) that's a loop
+# print (x)
 
 import turtle
 
 my_turtle = turtle.Turtle()
 length = 70
 angle = 90
+
 def square(length, angle):
 
   my_turtle.forward(length)
@@ -18,20 +19,10 @@ def square(length, angle):
   my_turtle.forward(length)
   my_turtle.right(angle)
 
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
-square(length, angle)
-my_turtle.right(45)
 
+for x in range(180):
+  square(length,angle)
+  my_turtle.right(2)
+  print(x)
+
+#Yey!!!! It worked! I created a loop!!!!

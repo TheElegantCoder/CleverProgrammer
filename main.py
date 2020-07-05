7114b678df14de19fce175fcd898821d704fff07
TheElegantCoder <katharina.rychnavska@gmail.com>
2020-07-05 08:13:12 +0000

    8 quadrate

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 # function with arguments, multiple arguments and loops 
+# for x in range(10)
+# print x
 
 import turtle
 
 my_turtle = turtle.Turtle()
-def square():
-  my_turtle.forward(100)
-  my_turtle.right(90)
-  my_turtle.forward(100)
-  my_turtle.right(90)
-  my_turtle.forward(100)
-  my_turtle.right(90)
-  my_turtle.forward(100)
-  my_turtle.right(90)
+length = 70
+angle = 90
+def square(length, angle):
+
+  my_turtle.forward(length)
+  my_turtle.right(angle)
+  my_turtle.forward(length)
+  my_turtle.right(angle)
+  my_turtle.forward(length)
+  my_turtle.right(angle)
+  my_turtle.forward(length)
+  my_turtle.right(angle)
+
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
+square(length, angle)
+my_turtle.right(45)
 

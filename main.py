b6d50170c212f6f0a585265feb00b1c1f475aa23
Katharina <katharina.rychnavska@gmail.com>
2020-07-06 11:50:33 +0000

    My 2nd Circle of Squares: Added speed to my turtle, deleted the last line

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# function with arguments, multiple arguments and loops 
-# for x in range(10) that's a loop
-# print (x)
 
 import turtle
 
 my_turtle = turtle.Turtle()
-length = 70
+length = 90
 angle = 90
+my_turtle.speed(0)
 
 def square(length, angle):
 
@@ -20,22 +18,7 @@ def square(length, angle):
   my_turtle.right(angle)
 
 
-for x in range(180):
+for x in range(100):
   square(length,angle)
-  my_turtle.right(2)
-  print(x)
-
-#Yey!!!! It worked! I created a loop!!!!
-
-# Qazi's Code for Circle of Squares:
-
-import turtle
-my_turtle = turtle.Turtle()
-my_turtle.speed(0)
-def square(length,angle)
-  for i in range(100)
-  square(100,90)
   my_turtle.right(11)
-
-#Circle has 360 degrees
-#360/10 -->36 We take the next undividible integer (11), so the squares are not overlapping.
+# deleted the line print(x) - it works perfectly 
